59c45846a26939d183f44e6d1e4109b1d457f68c
Said Maadan <saidmaadan@users.noreply.github.com>
2015-09-18 00:06:30 -0500

    Added backgound images candidates and employers index pages

--- config/routes.rb
+++ config/routes.rb
@@ -34,6 +34,9 @@ Rails.application.routes.draw do
   resources :companies do 
     resources :reviews, except: [:show, :index]
     resources :interviews, except: [:show, :index]
+    collection do
+      get 'search'
+    end
   end
   resources :insights
 
