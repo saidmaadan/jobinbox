ab22182cf5937affff3a5fdc705d09e940a4a197
Said Maadan <saidmaadan@users.noreply.github.com>
2015-10-10 10:30:34 -0500

    Display company images as carousel on home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,13 @@
 class PagesController < ApplicationController
   def home
+    @companies = Company.all
+    @jobs = Job.all
   end
 
   def about
+    @companies = Company.all
+    @jobs = Job.all
+    #@jobs = @company.jobs.paginate(:page => params[:page], :per_page => 3)
   end
 
   def terms
