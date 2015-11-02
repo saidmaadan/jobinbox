795b6de2dc2e735247a028464a415bc221c17a2a
Said Maadan <saidmaadan@users.noreply.github.com>
2015-11-02 15:10:41 -0600

    Modified page controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,12 @@
 class PagesController < ApplicationController
   def home
     @companies = Company.all
-    @jobs = Job.all
+    @jobs = Job.all.order("created_at DESC")
   end
 
   def about
     @companies = Company.all
-    @jobs = Job.all
+    @jobs = Job.all.order("created_at DESC")
     #@jobs = @company.jobs.paginate(:page => params[:page], :per_page => 3)
   end
 
